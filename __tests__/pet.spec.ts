9b07bb6b58bc8f7947940355a79ea42ad3fd72d6
icaro gavazza <icarodgl@gmail.com>
2020-03-17 01:49:43 +0000

    test(pet): initial implementation

--- __tests__/pet.spec.ts
+++ __tests__/pet.spec.ts
@@ -11,11 +11,37 @@ describe('GET /pet', () => {
   });
 });
 
-describe('POST /pet', () => {
+describe('CRUD pet', () => {
+  const pet = {
+    name: 'bugger',
+    foto: 'bugger.jpg',
+    porte: 'medio',
+    sobre: 'lindo',
+    idade: 1,
+    foiAdotado: false,
+  };
+  let pet_id = '';
   it('create pet', async () => {
     const res = await request(app)
       .post('/pet')
-      .send({ name: 'dog' });
+      .send(pet);
     expect(res.status).toBe(201);
+    expect(res.body).toMatchObject(pet);
+    pet_id = res.body._id;
+  });
+
+  it('update pet', async () => {
+    const res = await request(app)
+      .put(`/pet/${pet_id}`)
+      .send({ ...pet, name: 'bugguer da silva' });
+    expect(res.status).toBe(201);
+    expect(res.body).toMatchObject(pet);
+  });
+  it('delete pet', async () => {
+    const res = await request(app)
+      .put(`/pet/${pet_id}`)
+      .set('Accept', 'application/json');
+    expect(res.status).toBe(201);
+    expect(res.body).toMatchObject(pet);
   });
 });
