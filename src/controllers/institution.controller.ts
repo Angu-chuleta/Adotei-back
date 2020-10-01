ef794856d5b467df8e08f662c5ab9cd2f9ba707b
Tadeujr <tadeu_junior@outlook.com>
2020-10-01 19:33:54 -0300

    Add Controller Model, Service

---
+++ src/controllers/institution.controller.ts
@@ -0,0 +1,39 @@
+import { institutionService, InstitutionService } from '../services';
+import { BaseController } from './base.controller';
+import { IInstituitionModel } from '../models';
+
+class InstituitionController extends BaseController<
+  IInstituitionModel,
+  InstitutionService
+> {
+  constructor() {
+    super(institutionService, {
+      // keys do req.body que serão usados no create
+      create: [
+        'name',
+        'foto',
+        'email',
+        'telefone',
+        'sobre',
+        'credito',
+        'valido',
+        'termo',
+        'pets',
+      ],
+      // keys do req.body que serão usados no update
+      update: [
+        'name',
+        'foto',
+        'email',
+        'telefone',
+        'sobre',
+        'credito',
+        'valido',
+        'termo',
+        'pets',
+      ],
+    });
+  }
+}
+
+export const instituitionController = new InstituitionController();
