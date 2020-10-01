28dc2ce5a8d1c6cbfd814ae579402151ad3101eb
icarodgl <icarodgl@gmail.com>
2020-10-01 19:04:03 -0300

    commit da brotheragem

--- src/services/user.service.ts
+++
@@ -1,7 +0,0 @@
-import { UserModel, IUserModel } from '../models';
-import { BaseService } from './base.service';
-export class UserService extends BaseService<IUserModel> {
-  constructor() {
-    super(UserModel);
-  }
-}
